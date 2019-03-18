872b9c07678ada3ddc82fe611c7a04b59d7f44a0
Venkatarao Gude <venkatarao_gude@epam.com>
2019-03-18 16:35:06 +0530

    Added test

--- src/main/java/com/epam/dao/ProductRepository.java
+++ src/main/java/com/epam/dao/ProductRepository.java
@@ -2,9 +2,14 @@ package com.epam.dao;
 
 import com.epam.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    @Query("SELECT p.id FROM Product p" )
+    public List<Integer> getAllProductIds();
 }
